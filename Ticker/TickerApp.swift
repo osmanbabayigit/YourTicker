a13bbd7f10cfdb10e7e4d78a5302d6fe013a8546
osmanbabayigit <osmanbabayigi1@icloud.com>
2026-03-17 12:48:59 +0300

    Initial Commit

---
+++ Ticker/TickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickerApp.swift
+//  Ticker
+//
+//  Created by Osman Babayiğit on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
